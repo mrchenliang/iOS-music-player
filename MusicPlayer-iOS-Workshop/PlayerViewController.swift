598840fcbd121c2db337573f50d19b0ba658a3ea
mrchenliang <c.liang@ualberta.ca>
2019-11-06 09:12:44 -0500

    first commit

---
+++ MusicPlayer-iOS-Workshop/PlayerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MusicPlayer-iOS-Workshop
+//
+//  Created by Admin on 2016-09-09.
+//  Copyright © 2016 LHL. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController, MediaPlayerProtocol {
+    
+    @IBOutlet var mediaPlayer: MediaPlayer!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        mediaPlayer.setup()
+        
+    }
+    
+    func displaySong(_ song: Song) {
+      imageView.image = song.image
+    }
+    
+    func displayCompletionPercentage(_ percentComplete: Float) {
+      progressView.progress = percentComplete
+    }
+}
+
